d374b386d2a75556ed5d58f97ec35c2f0be449db
AI.Tsingh.ChiangC <jiangchuna@126.com>
2019-11-09 20:59:38 +0800

    tv script generation

--- 03_generate_tv_script/helper.py
+++ 03_generate_tv_script/helper.py
@@ -11,7 +11,7 @@ def load_data(path):
     Load Dataset from File
     """
     input_file = os.path.join(path)
-    with open(input_file, "r") as f:
+    with open(input_file, "r",encoding='UTF-8') as f:
         data = f.read()
 
     return data
